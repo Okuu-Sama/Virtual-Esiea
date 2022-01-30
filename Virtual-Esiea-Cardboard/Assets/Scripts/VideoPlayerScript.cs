7adf2a5004ef165b3893d87b48d963a92331802c
StellaTham <stella.thammavong@gmail.com>
2022-01-30 19:35:54 +0100

    Corrections of the video script and tests

--- Virtual-Esiea-Cardboard/Assets/Scripts/VideoPlayerScript.cs
+++ Virtual-Esiea-Cardboard/Assets/Scripts/VideoPlayerScript.cs
@@ -5,50 +5,13 @@ using UnityEngine.Video;
 
 public class VideoPlayerScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject videoScreen;
+    private VideoPlayer videoPlayer;
 
-    VideoPlayer videoPlayer;
-    // Start is called before the first frame update
     void Start()
     {
-        // Will attach a VideoPlayer to the main camera.
-        GameObject videoScreen = GameObject.Find("VideoPlayer");
-
-        // VideoPlayer automatically targets the camera backplane when it is added
-        // to a camera object, no need to change videoPlayer.targetCamera.
         videoPlayer = videoScreen.GetComponent<UnityEngine.Video.VideoPlayer>();
-
-        // Play on awake defaults to true. Set it to false to avoid the url set
-        // below to auto-start playback since we're in Start().
-        /*videoPlayer.playOnAwake = false;
-        videoPlayer.waitForFirstFrame = true;
-        // By default, VideoPlayers added to a camera will use the far plane.
-        // Let's target the near plane instead.
-        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
-        MeshRenderer renderer = camera.GetComponent<MeshRenderer>();
-        videoPlayer.targetMaterialRenderer = renderer;
-        videoPlayer.targetMaterialProperty = "_MainTex";
-        // This will cause our Scene to be visible through the video being played.
-        //videoPlayer.targetCameraAlpha = 0.5F;
-
-        // Set the video to play. URL supports local absolute or relative paths.
-        // Here, using absolute.
-        videoPlayer.url = "/Assets/Videos/dies from cringe TheGreatAceAttorney NintendoSwitch.mp4";
-
-        
-        // Skip the first 100 frames.
-        //videoPlayer.frame = 100;
-
-        // Restart from beginning when done.
-        videoPlayer.isLooping = true;
-
-        // Each time we reach the end, we slow down the playback by a factor of 10.
-        videoPlayer.loopPointReached += EndReached;
-        */
-        // Start playback. This means the VideoPlayer may have to prepare (reserve
-        // resources, pre-load a few frames, etc.). To better control the delays
-        // associated with this preparation one can use videoPlayer.Prepare() along with
-        // its prepareCompleted event.*
-        
         
     }
 
@@ -62,13 +25,4 @@ public class VideoPlayerScript : MonoBehaviour
         videoPlayer.Stop();
     }
 
-    /*void EndReached(UnityEngine.Video.VideoPlayer vp)
-    {
-        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
-    }*/
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
